b7bfa6afa95c88a9239b7a77657802ca146d3092
SevenIslay <karabhirup.ak@gmail.com>
2021-08-20 13:51:51 +0530

    Solution to the Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    get <- function() x
+    set <- function(y)
+    { 
+        x <<- y
+        inv <- NULL
+    }
+    setsolve <- function(i){ inv <<- i }
+    getsolve <- function() inv
+    list( get = get, set = set, setsolve = setsolve, getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    if(is.null(x$getsolve())) x$setsolve(solve(x$get(), ...))
+    x$getsolve()
 }
